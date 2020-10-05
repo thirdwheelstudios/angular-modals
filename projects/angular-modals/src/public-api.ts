fa57c104ec3902d05e86ab1c1485e4af8a30c276
Phil Crossland <phil@thirdwheelstudios.com>
2020-10-05 22:29:08 +0100

    Added missing exports

--- projects/angular-modals/src/public-api.ts
+++ projects/angular-modals/src/public-api.ts
@@ -2,7 +2,9 @@
  * Public API Surface of angular-modals
  */
 
+export * from './lib/button-style';
 export * from './lib/modal.service';
+export * from './lib/modal-dialog';
 export * from './lib/modal-dialog.component';
 export * from './lib/modal-input-dialog.component';
 export * from './lib/modal-content-container.directive';
