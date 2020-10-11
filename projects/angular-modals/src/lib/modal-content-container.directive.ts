f292a56c4f218bcbaca3e716d387048e58e615f6
Phil Crossland <phil@thirdwheelstudios.com>
2020-10-11 15:18:29 +0100

    Changed prefix from 3w to tw

--- projects/angular-modals/src/lib/modal-content-container.directive.ts
+++ projects/angular-modals/src/lib/modal-content-container.directive.ts
@@ -2,7 +2,7 @@ import { Directive, ViewContainerRef, OnInit } from '@angular/core';
 import { ModalService } from './modal.service';
 
 // tslint:disable-next-line:directive-selector
-@Directive({ selector: '[3w-modal]' })
+@Directive({ selector: '[tw-modal]' })
 export class ModalContentContainerDirective implements OnInit {
 
   constructor(private readonly vcr: ViewContainerRef, private readonly modalService: ModalService) {
